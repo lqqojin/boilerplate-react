cbac3a28d9670f9310ff2d728a30ee17f0d95c25
lqqojin <lqqojin@gmail.com>
2023-03-03 20:50:41 +0900

    [bug] require 로 변경 및 이미지 변경
    import 에러 발생으로 require로 변경

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
-import { override, addWebpackAlias } from 'customize-cra';
-import path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { override, addWebpackAlias } = require('customize-cra');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = override(
   addWebpackAlias({
--- src/components/LoginImage.tsx
+++ src/components/LoginImage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LoginImage() {
   return (
     <div className="float-left relative w-96 h-96">
-      <img src="images/login/Apple-macOS-13-ventura-wallpaper.jpg" alt="" />
+      <img src="images/login/login-test.png" alt="" />
     </div>
   );
 }
